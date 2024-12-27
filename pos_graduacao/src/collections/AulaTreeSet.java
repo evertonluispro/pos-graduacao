24c1892dab24a51e36f5aaf436697fdaa49ae69a
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-12-27 14:06:38 -0300

    Add files via upload

---
+++ pos_graduacao/src/collections/AulaTreeSet.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.util.TreeSet;
+
+public class AulaTreeSet {
+
+	public static void main(String[] args) {
+		
+		TreeSet<Integer> numeros = new TreeSet<>();
+		numeros.add(8);
+		numeros.add(6);
+		numeros.add(9);
+		
+		System.out.println("TreeSet: " + numeros);
+		
+		boolean result = numeros.remove(6);
+		System.out.println("Removeu??? " + result);
+		
+		result = numeros.removeAll(numeros);
+		System.out.println("TreeSet: " + result);
+
+	}
+
+}
